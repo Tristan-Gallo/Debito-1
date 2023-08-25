837124dfd43455e79825dc93f531dc223295f803
Tristan-Gallo <gallo.tristan@iisgalvanimi.edu.it>
2023-08-25 14:35:15 +0000

    pokemon e bootsrap

--- pokemon/src/app/app.module.ts
+++ pokemon/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchPokemonComponent } from './search-pokemon/search-pokemon.component';
 import { ItemPokemonComponent } from './item-pokemon/item-pokemon.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ItemPokemonComponent } from './item-pokemon/item-pokemon.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- pokemon/src/app/item-pokemon/item-pokemon.component.ts
+++ pokemon/src/app/item-pokemon/item-pokemon.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router'; // Importa Router
+
 import { PokemonService } from '../pokemon.service';
 
 @Component({
@@ -11,14 +12,22 @@ export class ItemPokemonComponent implements OnInit {
   pokemonId: number | undefined;
   pokemonDetails: any;
 
-  constructor(private route: ActivatedRoute, private pokemonService: PokemonService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router, // Inietta Router
+    private pokemonService: PokemonService
+  ) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      this.pokemonId = +params['id']; // Converto l'id in un numero
+      this.pokemonId = +params['id'];
       this.pokemonService.getPokemonDetailsById(this.pokemonId).subscribe(data => {
         this.pokemonDetails = data;
       });
     });
+  }
+
+  goBack() {
+    this.router.navigate(['/search']); // Torna alla pagina di ricerca
   }
 }
